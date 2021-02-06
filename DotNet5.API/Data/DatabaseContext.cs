ede0ca5bf816349a8971139e57a4d6fc715e88bc
smsmudit <smsmudit@gmail.com>
2021-02-06 23:30:10 +0530

    Add project files.

---
+++ DotNet5.API/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5.API.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Country> countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
---
+++ DotNet5.API/Startup.cs
@@ -0,0 +1,69 @@
+using DotNet5.API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DatabaseContext>(x =>
+                         x.UseSqlServer(Configuration.GetConnectionString("sqlConnection"))
+            );
+            services.AddCors(a=> {
+                a.AddPolicy("AllowAll", builder =>
+                         builder.AllowAnyOrigin()
+                         .AllowAnyHeader()
+                         .AllowAnyMethod()
+                );
+            });
+            services.AddSwaggerGen(a => 
+            {
+                a.SwaggerDoc("v1", new OpenApiInfo { Title = "Dotnet5.API", Version = "v1" });
+            });
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>c.SwaggerEndpoint("/swagger/v1/swagger.json", "Dotnet5.API v1"));
+            app.UseHttpsRedirection();
+            app.UseCors("AllowAll");
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
